c92923e76d1ce2d299d783af2295d8061a227742
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-25 16:22:19 +0200

    MongoDb+NodeJS+ExpressJS

---
+++ JavaScript/ToDoList/controllers/task.js
@@ -0,0 +1,46 @@
+const Task = require('../models/Task');
+
+module.exports = {
+    index: (req, res) => {
+        Task.find().then(tasks => {
+            res.render('task/index', {'tasks': tasks});
+        });
+
+    },
+
+    createGet: (req, res) => {
+        res.render('task/create');
+    },
+
+    createPost: (req, res) => {
+        let task = req.body;
+
+        Task.create(task).then(task => {
+            res.redirect('/');
+        }).catch(err => {
+            task.error="Cannot create task!";
+            res.render('task/create', task)
+        });
+    },
+
+    deleteGet: (req, res) => {
+        let taskId = req.params.id;
+
+        Task.findById(taskId).then(task => {
+       	if (task){
+       	    res.render('task/delete',task)
+        } else {
+       	    res.redirect('/')
+        }
+        }).catch(err=>res.redirect('/'))
+    },
+
+    deletePost: (req, res) => {
+        let taskId = req.params.id;
+
+        Task.findByIdAndRemove(taskId).then(task => {
+            res.redirect('/')
+        }).catch(err=>res.redirect('/'))
+
+    }
+};
