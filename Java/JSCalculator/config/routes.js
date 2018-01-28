b8799143e4465ef4e28039e384116bdd10f53ce2
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-28 12:26:57 +0200

    Delete routes.js

--- Java/JSCalculator/config/routes.js
+++
@@ -1,7 +0,0 @@
-const homeController = require('./../controllers/home');
-
-module.exports = (app) => {
-    app.get('/', homeController.indexGet);
-    app.post('/',homeController.indexPost);
-};
-
