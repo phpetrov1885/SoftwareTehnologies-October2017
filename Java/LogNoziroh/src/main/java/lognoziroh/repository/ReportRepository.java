cd907b313128500e97a238376343a9068b49abd1
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-02-14 14:51:48 +0200

    Delete ReportRepository.java

--- Java/LogNoziroh/src/main/java/lognoziroh/repository/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package lognoziroh.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import lognoziroh.entity.Report;
-
-public interface ReportRepository extends JpaRepository<Report, Integer> {
-}
