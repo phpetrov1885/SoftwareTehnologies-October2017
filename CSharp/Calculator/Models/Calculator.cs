9099e8653ccf523f733061d23ef7bb89b1b8f094
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-02-14 14:12:56 +0200

    Calculator

---
+++ CSharp/Calculator/Models/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator_CSharp.Models
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            this.Result = 0m;
+        }
+        public decimal RightOperand { get; set; }
+
+        public string Operator { get; set; }
+        public decimal LeftOperand { get; set; }
+        public decimal Result { get; set; }
+    }
+}
